ddfcf6a12a2a8eb79e3333cfef0d1ca5a8fdd7fd
Chen Li-Hsin <41140021+ChenLiHsin@users.noreply.github.com>
2020-12-29 12:36:43 +0800

    Update covid.js
    Add 14-disease

--- covid.js
+++ covid.js
@@ -1,41 +1,63 @@
-let model = tf.loadGraphModel('./web_model/model.json');
+let model_covid = tf.loadGraphModel('./web_model_covid/model.json');
+let model_14 = tf.loadGraphModel('./web_model_14/model.json');
 let inputElement = document.getElementById('fileInput');
 let imgElement = document.getElementById('original');
-let result_list = [document.getElementById('original_result'),
-                   document.getElementById('gaussian_result'),
-                   document.getElementById('gamma_result'),
-                   document.getElementById('sharpen_result')]
+let result_list = document.getElementById('original_result')
 
-let class_name=['Not covid-19', 'Covid']
 
 inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
     imgElement.style.visibility = "visible";
 }, false);
 
+let need_14 = false ;
+
 imgElement.onload = function() {
+    need_14 = false ;
     let img = tf.browser.fromPixels(imgElement);
     img = tf.image.resizeBilinear(img, [224, 224]);
     let test_data = tf.expandDims(img);
     test_data = test_data.div(tf.scalar(255));
-    predict(test_data,0);
     
-    let gamma_picture = adjust_gamma(1.5) ;
-    gamma_picture = tf.image.resizeBilinear(gamma_picture, [224, 224]);
-    test_data = tf.expandDims(img);
-    test_data = test_data.div(tf.scalar(255));
-    predict(test_data,1);
+    predict_covid(test_data) ;
+    
+    setTimeout(1000);
+    
+    predict_14(test_data) ;
+    
 }
 
-/* tensorflow predict API: https://js.tensorflow.org/api/latest/#tf.GraphModel.predict */
-async function predict(test_data,id) {
-    model.then(function(loadedModel) {
+async function predict_covid(test_data) {
+    model_covid.then(function(loadedModel) {
         const prediction = loadedModel.predict(test_data).flatten();
-        /* dataSync() gets the value for tf tensor object */
-        console.log(prediction.dataSync()) ;
-        let str = class_name[prediction.argMax().dataSync()[0]];
-        result_list[id].innerHTML = str;
+        console.log( "covid test" )
+        console.log( prediction.dataSync() )
+        
+        if ( prediction.dataSync()[0] <= prediction.dataSync()[1] ) {
+            result_list.innerHTML = "This images might has \"Covid\".";
+        }
+        else {
+            need_14 = true ;
+            result_list.innerHTML = "This images might \"NOT\" has Covid.";
+        }
     });
+}
+
+async function predict_14(test_data) {
+    model_14.then(function(loadedModel) {
+        const prediction = loadedModel.predict(test_data).flatten();
+        console.log( "14 test" )
+        console.log( prediction.dataSync() )
+                  
+        if ( need_14 ) {
+            result_list.innerHTML = "1.";
+        }
+    });
+}
+
+async function onOpenCvReady() {
+    document.querySelector('#status').innerHTML = 'opencv.js & all models is ready.';
+    inputElement.disabled = false ;
 }
 
 function adjust_gamma(gamma){
@@ -54,11 +76,4 @@ function adjust_gamma(gamma){
     }
     cv.imshow("gamma", mat);
     return mat ;
-}
-
-async function onOpenCvReady() {
-    document.querySelector('#status').innerHTML = 'opencv.js & all models is ready.';
-    
-    /* enable the button */
-    inputElement.disabled = false ;
 }
