e84dfbde2a8a97ff328f4a742729710c4f61a988
Chen Li-Hsin <41140021+ChenLiHsin@users.noreply.github.com>
2020-12-31 16:12:21 +0800

    Update covid.js
    Add 14-disease result output.

--- covid.js
+++ covid.js
@@ -15,8 +15,8 @@ inputElement.addEventListener('change', (e) => {
 
 let need_14 = false;
 
-document.getElementById("checkCorrect").addEventListener('click', (e) => addHistory(e, true, need_14));
-document.getElementById("checkWrong").addEventListener('click', (e) => addHistory(e, false, need_14));
+document.getElementById("checkCorrect").addEventListener('click', (e) => addHistory(e, true));
+document.getElementById("checkWrong").addEventListener('click', (e) => addHistory(e, false));
 
 imgElement.onload = function () {
     need_14 = false;
@@ -36,7 +36,7 @@ imgElement.onload = function () {
 
 }
 
-function addHistory(event, correct, isCovid) {
+function addHistory(event, correct) {
     checkCorrectBottom.disabled = true;
     checkWrongBottom.disabled = true;
     let image = document.getElementById("original");
@@ -50,19 +50,19 @@ function addHistory(event, correct, isCovid) {
     pictures.insertBefore(canvas, pictures.childNodes[0]);
     ctx.drawImage(image, 0, 0, image.width * 0.8, image.height * 0.8);
     ctx.font = "30px Arial";
-    if (isCovid && correct) {
+    if (!need_14 && correct) {
         ctx.fillText("Covid-19", 100, 150, 100);
         canvas.style.border = "green 10px solid"
     } // if()
-    else if (!isCovid && correct) {
+    else if (need_14 && correct) {
         ctx.fillText("Not Covid-19", 100, 150, 100);
         canvas.style.border = "green 10px solid"
     } // else if()
-    else if (isCovid && !correct) {
+    else if (!need_14 && !correct) {
         ctx.fillText("Covid-19", 100, 150, 100);
         canvas.style.border = "red 10px solid"
     } // else if()
-    else if (!isCovid && !correct) {
+    else if (need_14 && !correct) {
         ctx.fillText("Not Covid-19", 100, 150, 100);
         canvas.style.border = "red 10px solid"
     } // else if()
@@ -89,9 +89,78 @@ async function predict_14(test_data) {
         const prediction = loadedModel.predict(test_data).flatten();
         console.log("14 test")
         console.log(prediction.dataSync())
-
+        let result_str = "But this image has high percentage with";
         if (need_14) {
-            result_list2.innerHTML = "1.";
+            if (prediction.dataSync()[0] > 0.107) result_str = result_str + " \"Atelectasis\"";
+            if (prediction.dataSync()[1] > 0.029) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Cardiomegaly\"";
+            }
+
+            if (prediction.dataSync()[2] > 0.122) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Effusion\"";
+            }
+
+            if (prediction.dataSync()[3] > 0.192) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Infiltration\"";
+            }
+
+            if (prediction.dataSync()[4] > 0.052) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Mass\"";
+            }
+
+            if (prediction.dataSync()[5] > 0.059) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Nodule\"";
+            }
+
+            if (prediction.dataSync()[6] > 0.013) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Pneumonia\"";
+            }
+
+            if (prediction.dataSync()[7] > 0.047) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Pneumothorax\"";
+            }
+
+            if (prediction.dataSync()[8] > 0.043) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Consolidation\"";
+            }
+
+            if (prediction.dataSync()[9] > 0.023) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Edema\"";
+            }
+
+            if (prediction.dataSync()[10] > 0.018) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Emphysema\"";
+            }
+
+            if (prediction.dataSync()[11] > 0.012) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Fibrosis\"";
+            }
+
+            if (prediction.dataSync()[12] > 0.026) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Pleural_Thickening\"";
+            }
+
+            if (prediction.dataSync()[13] > 0.0018) {
+                if (result_str != "But this image has high percentage with") result_str = result_str + ","
+                result_str = result_str + " \"Hernia\"";
+            }
+
+            if (result_str == "But this image has high percentage with") result_str = "";
+
+            // result_list.innerHTML = "This image might \"NOT\" has Covid.\n" + result_str;
+            result_list2.innerHTML = result_str;
         }
     });
 }
